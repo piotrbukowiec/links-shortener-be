a18861273563d836ebf36a5f08011a0fbf84005f
Unmarked4683 <pbuk60@gmail.com>
2024-06-07 15:33:13 +0200

    Added db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,29 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LinksModule } from './links/links.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ConfigModule.forRoot(), LinksModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: configService.get<'postgres'>('DB_TYPE'),
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: 'links_shortener',
+        entities: [__dirname + '/**/*.entity{.js,.ts}'],
+        logging: true,
+        synchronize: true,
+      }),
+    }),
+    LinksModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
