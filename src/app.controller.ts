fb5ab43d30e910ebb5ac72aee06ed4284ac6e873
Unmarked4683 <pbuk60@gmail.com>
2024-06-07 10:25:13 +0200

    Added .env, .env.example added to repo

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpCode, HttpStatus } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('/')
+  @HttpCode(HttpStatus.OK)
+  async getHello(): Promise<string> {
+    return await this.appService.getHello();
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
 
-async function bootstrap() {
+const bootstrap = async () => {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-}
+  const config = new DocumentBuilder()
+    .setTitle('Link shortener documentation')
+    .setDescription('Documentation of link shortener app')
+    .setVersion('1.0')
+    .build();
+
+  const configService = app.get(ConfigService);
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+  await app.listen(configService.get<number>('APP_PORT'));
+};
 bootstrap();
