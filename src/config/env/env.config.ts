0bf7c316c6c2bf0165c8bd657ebef65894e228ba
Unmarked4683 <pbuk60@gmail.com>
2024-06-11 22:47:31 +0200

    Added .env.default, add multiple files to .env config

--- src/config/env/env.config.ts
+++ src/config/env/env.config.ts
@@ -2,4 +2,5 @@ import { ConfigModuleOptions } from '@nestjs/config';
 
 export const envConfig: ConfigModuleOptions = {
   isGlobal: true,
+  envFilePath: ['.env', '.env.default', '.env.example'],
 };
